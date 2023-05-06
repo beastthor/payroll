5ec763e3e9d3da90c96ac93751b1a6e40f49a2d0
kishan_ykc <kishan.ykc@gmail.com>
2023-05-06 13:08:59 -0400

    first commit added new files

---
+++ src/main/java/dev/kishan/payroll/Employee.java
@@ -0,0 +1,76 @@
+package dev.kishan.payroll;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+
+
+
+
+@Entity
+class Employee {
+
+    private @Id
+    @GeneratedValue Long id;
+    private String name;
+    private String role;
+
+    Employee(){}
+
+Employee(String name, String role){
+
+        this.name = name;
+        this.role = role;
+}
+
+public Long getId(){
+        return this.id;
+}
+
+public String getName(){
+        return this.name;
+}
+
+public String getRole(){
+        return this.role;
+}
+
+public void setId(Long id){
+        this.id = id;
+}
+
+public void setName(String name){
+        this.name = name;
+}
+
+public void setRole(String role){
+        this.role=role;
+}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) && Objects.equals(name, employee.name) && Objects.equals(role, employee.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, role);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
+
